2ffa98a66286b1a114c85b1af08cfdc802ef56de
Swiichy <adlpromail@gmail.com>
2023-05-16 22:04:52 +0200

    .

--- src/controllers/webToken.js
+++ src/controllers/webToken.js
@@ -1,12 +1,13 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const { TOKEN_EXPIRATION } = require("../constants");
 require("dotenv").config();
 
 exports.generateToken = (req, res) => {
   const token = jwt.sign(
     { userId: "arbitrary_user_id" },
     process.env.JWT_SECRET,
-    { expiresIn: "30d" }
+    { expiresIn: TOKEN_EXPIRATION }
   );
 
   mongoose.connection.collection("tokens").insertOne({ token });
